cb4ea2f3e0a74c433ab3c9c3cd5884e981824817
Cornal <nguyendangduy2210@gmail.com>
2022-02-02 00:41:53 +0700

    done

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const cors = require('cors');
+const { addUser, getUsersInRoom, removeUser, getUser } = require('./users')
+const router = require('./router');
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = socketIO(httpServer);
+
+app.use(cors());
+app.use(router);
+
+io.on('connect', (socket) => {
+    socket.on('join', ({ name, room }, callback) => {
+        const { user, error } = addUser({ id: socket.id, name, room });
+        if(error) return callback(error);
+
+        socket.join(room);
+        socket.emit("message", { user: 'admin', text: `${user.name} welcome to room ${user.room}` });
+        socket.broadcast.to(user.room).emit("message", { user: 'admin', text: `${user.name} has joined!` });
+        io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
+        callback();
+    })
+
+    socket.on('sendMessage', () => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', { user: user.name, text: message });
+        callback();
+    })
+
+    socket.on("disconnect", () => {
+        const user = removeUser(socket.id);
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
+    })
+})
+
+httpServer.listen(process.env.PORT || 3000, () => console.log(`Server has started.`));
