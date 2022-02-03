82ef7dea7b644430a5dbbdeceaee36150eff6f40
Cornal <nguyendangduy2210@gmail.com>
2022-02-04 00:02:08 +0700

    done proj

--- server/index.js
+++ server/index.js
@@ -18,12 +18,12 @@ io.on('connect', (socket) => {
         if(error) return callback(error);
         socket.join(room);
         socket.emit("message", { user: 'admin', text: `${user.name} welcome to room ${user.room}` });
-        // socket.broadcast.to(user.room).emit("message", { user: 'admin', text: `${user.name} has joined!` });
-        // io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
-        // callback();
+        socket.broadcast.emit("message", { user: 'admin', text: `${user.name} has joined!` });
+        io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
+        callback();
     })
 
-    socket.on('sendMessage', () => {
+    socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
         io.to(user.room).emit('message', { user: user.name, text: message });
         callback();
